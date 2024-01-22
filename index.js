c6db9ce40fa7ce5f90f203f0612c485160a64d6a
suraj <surajjosh7@gmail.com>
2024-01-22 21:55:41 +0530

    updated

--- index.js
+++
@@ -1,5 +0,0 @@
-const ansi= require("./src/ansi");
-const tw_back=require("./src/typewritter_backward");
-const tw_forward=require("./src/typewritter_forward");
-
-module.exports={ansi,tw_back,tw_forward}
