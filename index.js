15eb7ad95a0d60786f948ff6568572867023e509
suraj <surajjosh7@gmail.com>
2024-01-23 17:30:18 +0530

    updated

---
+++ index.js
@@ -0,0 +1,7 @@
+const ansi=require("./src/ansi");
+const fade=require("./src/fade");
+const karaoke=require("./src/karaoke");
+const tw_backword=require("./src/typewritter_backward");
+const tw_forward=require("./src/typewritter_forward");
+
+module.exports={ansi,fade,karaoke,tw_backword,tw_forward};
